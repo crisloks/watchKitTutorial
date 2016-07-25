8f9800e99c9b937242ca14f836be716343d5f128
Cristián Torres <cristian.torres@mercadolibre.cl>
2016-07-25 02:59:15 -0400

    Flight inside the schedule changes color when checked in

--- Watch Extension/ScheduleInterfaceController.swift
+++ Watch Extension/ScheduleInterfaceController.swift
@@ -13,6 +13,7 @@ class ScheduleInterfaceController: WKInterfaceController {
     @IBOutlet var flightsTable: WKInterfaceTable!
     
     var flights = Flight.allFlights()
+    var selectedIndex = 0
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -25,9 +26,19 @@ class ScheduleInterfaceController: WKInterfaceController {
         }
     }
     
+    override func didAppear() {
+        super.didAppear()
+        if flights[selectedIndex].checkedIn, let rowController = flightsTable.rowControllerAtIndex(selectedIndex) as? FlightRowController {
+            animateWithDuration(0.35, animations: { () -> Void in
+                rowController.updateForCheckIn()
+            })
+        }
+    }
+    
     override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
         let flight = flights[rowIndex]
         let controllers = [Constants.kFlightController, Constants.kCheckInController]
+        selectedIndex = rowIndex
         presentControllerWithNames(controllers, contexts: [flight, flight])
     }
 
