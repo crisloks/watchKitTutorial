e248b65758c5011ad543d53b35fa6054ad5e7346
Cristián Torres <cristian.torres@mercadolibre.cl>
2016-07-24 22:10:24 -0400

    Connected interface to date (Inital implementation)

---
+++ Watch Extension/FlightInterfaceController.swift
@@ -0,0 +1,40 @@
+
+import WatchKit
+import Foundation
+
+
+class FlightInterfaceController: WKInterfaceController {
+    @IBOutlet var flightLabel: WKInterfaceLabel!
+    @IBOutlet var routeLabel: WKInterfaceLabel!
+    @IBOutlet var boardingLabel: WKInterfaceLabel!
+    @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var gateLabel: WKInterfaceLabel!
+    @IBOutlet var seatLabel: WKInterfaceLabel!
+    
+    var flight: Flight? {
+        didSet {
+            if let flight = flight {
+                flightLabel.setText("Flight \(flight.shortNumber)")
+                routeLabel.setText(flight.route)
+                boardingLabel.setText("\(flight.number) Boards")
+                boardTimeLabel.setText(flight.boardsAt)
+                
+                if flight.onSchedule {
+                    statusLabel.setText("On Time")
+                } else  {
+                    statusLabel.setText("Delayed")
+                    statusLabel.setTextColor(UIColor.redColor())
+                }
+                gateLabel.setText("Gate \(flight.gate)")
+                seatLabel.setText("Seat \(flight.seat)")
+            }
+        }
+    }
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        flight = Flight.allFlights().first!
+    }
+
+}
